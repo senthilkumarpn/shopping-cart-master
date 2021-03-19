8af61736ac7a946e8a5e65c666c8923406366a2d
Rangarajan <rangarajan1234.ramadasscfghgsdfsg@cognizant.com>
2021-03-19 12:29:14 +0530

    Fix for remediation :FileSystem-Write-FileWriter - Write and Append - 1

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -2,17 +2,28 @@ package org.ejb.sample.servlet.helper;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.s3.*;
 
 public class FileUtility {
 
-        void generateReportFile(String content) throws IOException {
+    void generateReportFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
+        fileWriter.write(content);
+        fileWriter.close();
+        uploadS3ObjectAndGetUrl("myBucket", "~/DailyReports/Report.csv", "Report.csv");
+    }
 
-            FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
-            fileWriter.write(content);
-            fileWriter.close();
-        }
+    public AmazonS3 getS3Client() {
+        AmazonS3 s3client;
+        AWSCredentials credentials = new BasicAWSCredentials(System.getenv("AWS_S3_ACCESS_KEY"), System.getenv("AWS_S3_SECRET_KEY"));
+        s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(System.getenv("AWS_S3_CLIENT_REGION")).build();
+        return s3client;
+    }
 
+    public URL uploadS3ObjectAndGetUrl(String bucketName, String uploadContentPath, String keyName) {
+        AmazonS3 s3client = getS3Client();
+        s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
+        return s3client.getUrl(bucketName, keyName);
+    }
 }
-
-
-
